5cb698c099f221be7b6e666d9b3fe4c152320df3
Daria Tsypkina <dariatsypkina@gmail.com>
2023-02-05 20:04:23 +0300

    feat: add base styles, config themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Suspense } from "react";
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
 import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
+import { useTheme } from "./theme/useTheme";
+import { classnames } from "./helpers/classnames/classnames";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={classnames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>Change Theme</button>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
 
---
+++ src/helpers/classnames/classnames.ts
@@ -0,0 +1,22 @@
+type Mods = Record<string, boolean | string>;
+
+/**
+ * Create string of classnames using params
+ * @param cls - classname string
+ * @param mods - object with key/value pairs where key is classname and value is of bool or string type
+ * @param additional - an array of any onther classname strings
+ */
+export function classnames(
+  cls: string,
+  mods: Mods = {},
+  additional: string[]
+): string {
+  return [
+    cls,
+    ...additional,
+    ...Object.entries(mods)
+      .filter(([, value]) => !!value)
+      .map(([className]) => className),
+  ].join(" ");
+}
+
