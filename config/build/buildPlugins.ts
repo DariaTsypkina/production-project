44cf2b6585d191399c8747307b59ef60c9484fd3
Daria Tsypkina <dariatsypkina@gmail.com>
2023-02-12 20:56:52 +0300

    build: add hot module replacement and refresh plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,12 +2,13 @@ import * as webpack from "webpack";
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HTMLWebpackPlugin({
       template: paths.html,
     }),
@@ -20,5 +21,12 @@ export function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev),
     }),
   ];
+
+  if (isDev) {
+    plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+
+  return plugins;
 }
 
