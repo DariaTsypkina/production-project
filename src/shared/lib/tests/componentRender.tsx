30ad689bc34a447b796d1214e89d0df7d0292f34
Daria Tsypkina <dariatsypkina@gmail.com>
2023-02-20 19:10:48 +0300

    test: fix component render decorator

--- src/shared/lib/tests/componentRender.tsx
+++ src/shared/lib/tests/componentRender.tsx
@@ -4,11 +4,19 @@ import { I18nextProvider } from "react-i18next";
 import { MemoryRouter } from "react-router-dom";
 import i18nForTests from "shared/config/i18n/i18nForTests";
 
-export function componentRender(component: ReactNode): RenderResult {
+export interface ComponentRenderOptions {
+  route?: string;
+}
+
+export function componentRender(
+  component: ReactNode,
+  options: ComponentRenderOptions = {}
+): RenderResult {
+  const { route = "/" } = options;
   return render(
-    <I18nextProvider i18n={i18nForTests}>
-      <MemoryRouter>{component}</MemoryRouter>
-    </I18nextProvider>
+    <MemoryRouter initialEntries={[route]}>
+      <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
+    </MemoryRouter>
   );
 }
 
