284d650b02e97f36904c16270820a6ff7752d5e0
Daria Tsypkina <dariatsypkina@gmail.com>
2023-02-25 19:42:10 +0300

    config: init husky

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -3,7 +3,6 @@ import { FC } from "react";
 import { classnames } from "shared/lib/classnames/classnames";
 import { Modal, ModalProps } from "shared/ui/Modal";
 import { LoginForm } from "../LoginForm/LoginForm";
-import s from "./LoginModal.module.scss";
 
 interface LoginModalProps extends ModalProps {
   className?: string;
@@ -13,11 +12,7 @@ export const LoginModal: FC<LoginModalProps> = (props) => {
   const { className, ...otherProps } = props;
 
   return (
-    <Modal
-      {...otherProps}
-      lazy
-      className={classnames(s.LoginModal, [className], {})}
-    >
+    <Modal {...otherProps} lazy className={classnames("", [className], {})}>
       <LoginForm isOpen={otherProps.isOpen} />
     </Modal>
   );
